6b3a65d5975325202020b54561453e7c58ecb61e
tgattaz <thomas.gattaz@gmail.com>
2019-10-09 11:14:31 +0200

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,87 @@
+import {Component, OnInit} from '@angular/core';
+import {Observable} from "rxjs/Observable";
+import {HttpClient, HttpEventType, HttpHeaders, HttpParams, HttpRequest} from "@angular/common/http";
+import * as _ from 'lodash';
+
+@Component({
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+
+    HAL$:  Observable<any>;
+
+    ARXIV$:  Observable<any>;
+
+    constructor(private http: HttpClient) {
+
+    }
+
+    ngOnInit() {
+    }
+
+
+    SearchHAL(filter: string) {
+
+
+        const headers = new HttpHeaders()
+            .set("X-CustomHeader", "custom header value");
+
+        const httpGet$ = this.http
+            .get("http://api.archives-ouvertes.fr/search/?q="+filter+"&wt=json", {headers})
+            .map(data => _.values(data))
+            .shareReplay();
+
+        httpGet$.subscribe(
+            (val) => console.log(val)
+        );
+
+        this.HAL$ = httpGet$;
+        this.ARXIV$ = null;
+
+    }
+
+    ParseString(data: string){
+        var result;
+        var parser = require('xml2js');
+        parser.Parser().parseString(data, (e, r) => {result = r});
+        return result;  
+    }
+
+
+    SearchARXIV(filter: string) {
+
+        const my_headers = new HttpHeaders({ 'Content-Type': 'application/xml' }).set('Accept', 'application/xml');
+        
+        const options: {
+            headers?: HttpHeaders,
+            observe?: 'body',
+            params?: HttpParams,
+            reportProgress?: boolean,
+            responseType: 'text',
+            withCredentials?: boolean
+        } = {
+            headers: my_headers,
+            responseType: 'text'
+        };
+
+        const httpGet$ = this.http
+            .get("http://export.arxiv.org/api/query?search_query="+filter,options)
+            .map(data => _.values(this.ParseString(data)))
+            .shareReplay();
+
+        httpGet$.subscribe(
+            (val) => console.log(val)
+        );
+
+        this.ARXIV$ = httpGet$;
+        this.HAL$ = null;
+
+        
+
+        //this.result$ = httpGet$;
+
+    }
+
+}
